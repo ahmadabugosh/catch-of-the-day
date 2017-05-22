5081b57858fe5898a5d13105ef93b56aa1a9455f
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-05-22 09:02:58 +0400

    Added react routes

---
+++ src/components/NotFound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class NotFound extends React.Component {
+
+	render() {
+
+		return (
+			<div>
+
+		 <br/>
+		 <h2> Error 404: Page not found </h2>
+
+		 </div>
+		 )
+	}
+
+}
+
+export default NotFound;
