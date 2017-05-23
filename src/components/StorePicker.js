9431730ad80b44388249a8cd1655df1754c93cda
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-05-23 19:51:09 +0400

    Added random store picker helper function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 
 class StorePicker extends React.Component {
@@ -10,7 +11,7 @@ class StorePicker extends React.Component {
 		 <form className="store-selector">
 		 	{/* Comment */}
 		 <h2> Please enter a store</h2>
-		 <input type="text" required placeholder="Store Name" />
+		 <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
 		 <button type="submit"> Visit Store </button>
 
 		 </form>
