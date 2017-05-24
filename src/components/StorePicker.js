51be9cf66c73f7ffb1cbc07e021db7eaf5e192a5
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-05-25 00:00:01 +0400

    Added dynmaic input handler

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,14 +4,22 @@ import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
 
+	goToStore (event) {
+
+		event.preventDefault();
+
+console.log("You changed the store");
+console.log(this.storeInput.value);
+	}
+
 	render() {
 
 		return (
 
-		 <form className="store-selector">
+		 <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
 		 	{/* Comment */}
 		 <h2> Please enter a store</h2>
-		 <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+		 <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={ (input) => {this.storeInput = input}} />
 		 <button type="submit"> Visit Store </button>
 
 		 </form>
