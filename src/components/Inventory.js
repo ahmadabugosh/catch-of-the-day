e8d9fe0275876ca1aebe7dbbb80562fa5bb17d69
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-05-27 11:54:02 +0400

    Loaded sample data

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -10,6 +10,8 @@ class Inventory extends React.Component {
 <div>
 		 <p> Inventory</p>
 		 <AddFishForm addFish={this.props.addFish} />
+		 <button onClick={this.props.loadSamples}>Load Samples</button>
+
 		 </div>
 		 )
 	}
