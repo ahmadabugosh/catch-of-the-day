84c245e279bfd332f17a31c6c9fedb778f24044f
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-05-29 21:15:27 +0400

    Added firebase config

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import Rebase from 're-base';
+
+const base =Rebase.createClass({
+	apiKey: "AIzaSyDKCwFh0p3_vV-mZIfds4akh1PY-hRfQrs",
+    authDomain: "catch-of-the-day-abugosh.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-abugosh.firebaseio.com"
+
+
+});
+
+export default base;
+
